1fefabaf6e6e69a850c8bcf07b4a90f1166765b3
Akira Hayakawa <ruby.wktk@gmail.com>
2014-10-16 17:34:24 +0900

    implement show
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- board.ml
+++ board.ml
@@ -1,9 +1,27 @@
-module Board = struct
-        type t = int array array
-        let make n = Array.make_matrix 0 (n+2) (n+2)
-        let show board = assert false
-        let put_stone (x, y) t = assert false
-end
+open Array
+type t = int array array
+let make n = make_matrix (n+2) (n+2) 2
+let show b = 
+  Printf.printf "   [1 2 3 4 5 6 7 8 9 10111213141516171819]\n" ;
+  let p i = match i with
+  | 0 -> '@'
+  | 1 -> 'O'
+  | _ -> ' '
+  in
+  for i = 1 to (length b) - 2 do
+    Printf.printf "%2d| " i;
+    for j = 1 to (length b) - 2 do
+      Printf.printf "%c " (p (get (get b i) j)) ;
+    done ;
+    print_newline () ;
+  done
+let put_stone t (i, j) a = set (get t i) j a
+let can_put t (i, j) = assert false
 
-let b = Board.make 19 ;;
-Board.show b
+let b = make 19 ;;
+put_stone b (4, 4) 0 ;
+put_stone b (16, 16) 1 ;
+put_stone b (17, 14) 0 ;
+put_stone b (14, 16) 1 ;
+put_stone b (16, 10) 0 ;
+show b ;
