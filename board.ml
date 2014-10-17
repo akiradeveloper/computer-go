3a804c1535eb64972f22f0a4e870629ffcc6c720
Akira Hayakawa <ruby.wktk@gmail.com>
2014-10-17 18:01:23 +0900

    implement do_remove_stones
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- board.ml
+++ board.ml
@@ -15,13 +15,11 @@ let make n =
     done
   done ;
   b
-;;
 
 let flip_color = function
   | 0 -> 1
   | 1 -> 0
   | _ -> assert false
-;;
 
 let show b = 
   Printf.printf "   [1 2 3 4 5 6 7 8 9 10111213141516171819]\n" ;
@@ -39,13 +37,11 @@ let show b =
     done ;
     print_newline () ;
   done
-;;
 
 (* let put_stone t (i, j) a = set (get t i) j a *)
 (* ;; *)
 let put_stone t (i, j) a =
   t.(i).(j) <- a
-;;
 
 (* do_put_stones can be implemented in either way *)
 (* let rec do_put_stones t xs = *)
@@ -58,7 +54,6 @@ let put_stone t (i, j) a =
 (* ;; *)
 let do_put_stones t xs =
   List.iter (fun (i, j, a) -> put_stone t (i, j) a) xs
-;;
 
 let put_stones t xs =
   let rec zip xs ys =
@@ -68,8 +63,19 @@ let put_stones t xs =
     | ((i,j) :: xs', a :: ys') -> (i, j, a) :: (zip xs' ys') in
   let alt_color xs = zip xs (List.mapi (fun i _ -> i mod 2) xs) in
   do_put_stones t @@ alt_color xs
-;;
 
+let out_board t = function
+  | (0, _) | (20, _) | (_, 0) | (_, 20) -> false
+  | _ -> true
+
+let pos2int (i, j) = (i lsl 5) + j
+let int2pos n = (n lsr 5, n land 31)
+
+let search_hole t (i, j) =
+  if t.(i+1).(j) == 3 then true else
+    if t.(i-1).(j) == 3 then true else
+      if t.(i).(j+1) == 3 then true else
+        if t.(i).(j-1) == 3 then true else false
 
 module IntSet = Set.Make(
   struct
@@ -77,16 +83,35 @@ module IntSet = Set.Make(
     type t = int
   end)
 ;;
+
 (* TODO *)
-let do_remove_stones b (i, j, a) =
-  let s = IntSet.empty in
-  let to_int (i, j) = (i lsl 5) + j in
-  let visit (i, j, a) =
-    IntSet.add (to_int (i, j)) s in
-  ()
+let do_remove_stones b (i, j, color) =
+  let poss = ref [] in
+  let s = ref IntSet.empty in
+  let found_hole = ref false in
+  let rec visit (i, j, a) =
+    if a != color then ()
+    else
+      if out_board b (i, j) then ()
+      else
+        if !found_hole then ()
+        else
+          if search_hole b (i, j) then
+            found_hole := true 
+          else
+            if IntSet.mem (pos2int (i, j)) !s then
+              poss := (i, j) :: !poss
+            else
+              s := IntSet.add (pos2int (i, j)) !s ;
+              visit (i+1, j, flip_color a) ;
+              visit (i-1, j, flip_color a) ;
+              visit (i, j+1, flip_color a) ;
+              visit (i, j-1, flip_color a) ;
+  in
+  visit (i, j, color) ;
+  if !found_hole then [] else !poss
 
 let can_put t (i, j) = assert false
-;;
 
 let b = make 19 ;;
 
@@ -94,6 +119,6 @@ let b = make 19 ;;
 (* put_stones b l ; *)
 
 let l = [(10,10,0); (10,9,1); (10,11,1); (9,10,1); (11,10,1)] ;;
-do_put_stones b l;
-do_remove_stones b (10,10,0);
+do_put_stones b l ;
+do_remove_stones b (10,10,0) ;
 show b ;
