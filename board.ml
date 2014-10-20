1784d3b1c7783d873a91bd0a1ee4cf51cebb3f44
Akira Hayakawa <ruby.wktk@gmail.com>
2014-10-20 14:00:29 +0900

    Implement Ren (or string)
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- board.ml
+++ board.ml
@@ -41,7 +41,7 @@ let show t =
   let p i = match i with
   | 0 -> '@'
   | 1 -> 'O'
-  | 3 -> ' '
+  | 3 -> '+'
   | _ -> assert false
   in
   for i = 1 to (length t.matrix) - 2 do
@@ -226,10 +226,53 @@ show t ;
 (* ren *)
 
 type ren_attr = {
-  colo: int
+  color: int
 }
 
-let create_ren m =
-  let n = length m in 
-  let result = make_matrix n n (-1) in
+let create_ren mat =
+  let n = length mat in 
+  let result = make_matrix n n 0 in
+  let number = ref 1 in
+  let rec f (i, j, a) id =
+    if result.(i).(j) > 0 then
+      false
+    else
+      if mat.(i).(j) = a then (* outside can be eliminated by this check *)
+        begin
+          result.(i).(j) <- id ;
+          ignore @@ f (i+1, j, a) id ;
+          ignore @@ f (i-1, j, a) id ;
+          ignore @@ f (i, j+1, a) id ;
+          ignore @@ f (i, j-1, a) id ;
+          true
+        end
+      else
+        false
+  in
+  let g = 
+    for i = 1 to (n - 2) do
+      for j = 1 to (n - 2) do
+        let a = mat.(i).(j) in
+        if a < 3 then
+          if f (i, j, a) !number then
+            number := !number + 1 ;
+      done
+    done 
+  in
+  g ;
   result
+;;
+
+let show_ren t = 
+  Printf.printf "   [1 2 3 4 5 6 7 8 9 10111213141516171819]\n" ;
+  for i = 1 to (length t) - 2 do
+    Printf.printf "%2d| " i ;
+    for j = 1 to (length t) - 2 do
+      Printf.printf "%2d" @@ t.(i).(j) ;
+    done ;
+    print_newline () ;
+  done
+;;
+
+put_stone t (4,1,1) ;
+show_ren @@ create_ren t.matrix ;
