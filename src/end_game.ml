2da53550cd61b3207e0c42a35defe2e55eac9a18
Akira Hayakawa <ruby.wktk@gmail.com>
2014-11-07 13:57:43 +0900

    count
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/end_game.ml
+++ src/end_game.ml
@@ -1,4 +1,16 @@
 open Core.Std
 
-let finish board = assert false
-let count board = assert false
+let finish mat = assert false
+
+(* Chinese rule *)
+let count mat = 
+  let bw = Array.create 2 0 in
+  let sz = (Array.length mat) - 2 in
+  for i = 1 to sz do
+    for j = 1 to sz do
+      match mat.(i).(j) with
+      | 2 -> assert false
+      | 3 -> assert true
+      | _ as a -> bw.(a) <- bw.(a) + 1
+    done
+  done
